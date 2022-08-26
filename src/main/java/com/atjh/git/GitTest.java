4516d4a3d088f30ad1275312560d26dcb7eb9723
Ljh <Ljh@atjh.com>
2022-08-26 20:31:22 +0800

    hot-fix commit1

--- src/main/java/com/atjh/git/GitTest.java
+++ src/main/java/com/atjh/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[]args){
         System.out.println("hello erzi!");
         System.out.println("hello erzi222!");
+        System.out.println("hello erzi333!");
     }
 }
